7cec6de67f11da1bf5fcdbf199006ea0075a5dbd
jmalan210 <>
2026-05-20 18:29:22 -0600

    /recipes route showing expected data

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Recipe = require('../models/Recipe');
 
 
-router.get('/', (req, res) => {
-    res.send('Routes working');
+router.get('/', async (req, res) => {
+    try {
+        const recipes = await Recipe.find();
+        res.status(200).json(recipes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 
